98057e8ff3cc660ae400e0478523e8bd506278cf
Edmund Yu <eyu1@ualberta.ca>
2018-10-21 19:12:44 -0600

    Working cont.
    - Found out that i was working on a function that i wasnt supposed to do... LOL
    - Found pdf on what to do , so started showPage function
    SMH

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
-let studentList = document.querySelector('.student-list');
-let studentItem = document.querySelector('.student-item');
-let studentDetails = document.querySelector('.student-details');
-let studentJoined= document.querySelector('.joined-details');
+let studentList = document.querySelector('.student-list'); // pulls everything from list
+let studentItem = document.querySelectorAll('.student-item'); // pulls only the first student from list.
+let studentDetails = document.querySelectorAll('.student-details');
+let studentJoined= document.querySelectorAll('.joined-details');
 
-// let child = studentList.childNodes;
-// console.log (child);
-//console.log(studentList);
-//console.log(studentItem);
+//let child = studentList.childNodes;
+//console.log (child);
+// console.log(studentList);
+// console.log(studentItem);
 // console.log(studentDetails);
 // console.log(studentJoined);
 
@@ -20,22 +20,29 @@ let studentJoined= document.querySelector('.joined-details');
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 students, the last page will only display four
-function hideItems (number) {
-    let child = studentItem;
-    console.log(child);
-    let parent= studentItem.parentNode;
-    console.log(parent);
+// So i created a funciton thats not what I was supposed to do, but i dont wanna delete cause i spend a couple of hours LOL.
+// function hideItems (number) {
+//     let parent= studentList;
+//     //console.log(parent);
+//     let child = studentItem;
+//     //console.log(child);
+//   for (let i = 0; i < number ; i++) {
+//       parent.removeChild(child[i]);
+//     }
+//     return parent;
+// }
+// var result = hideItems(44);
+// //console.log(result);
 
-    for (let i = 0; i < number; i += 1) {
-       parent[i] = parent.removeChild(child);
+const showPage = (list, page) => {
+  for (let i = 0; i >= list.length; i++) {
+    if (i >= list[i]  && i <= list[i]) {// if the index of a list item is >= the index of the first item that should be shown on the page, && the list item index is <= the index of the last item that should be shown on the page,
+      return list;
+    } else {
+
     }
-
-    return parent;
+  }
 }
-
-var result = hideItems(52);
-//console.log(result);
-
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
 

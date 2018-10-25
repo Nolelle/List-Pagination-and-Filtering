5bc8156ec1c21ca5a3b9dcee9c60bec39eae4060
Edmund Yu <eyu1@ualberta.ca>
2018-10-24 18:39:26 -0600

    Working cont.
    -Implemented showPage function,
    -But console shows theres error, but function still runs ??????

--- js/script.js
+++ js/script.js
@@ -5,18 +5,10 @@ FSJS project 2 - List Filter and Pagination
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
 let studentList = document.querySelector('.student-list'); // pulls everything from list
-let studentItem = document.querySelectorAll('.student-item'); // pulls only the first student from list.
+let parent = studentList;
+let studentItem = document.querySelectorAll('.student-item');// pulls only the first student from list.
 let studentDetails = document.querySelectorAll('.student-details');
 let studentJoined= document.querySelectorAll('.joined-details');
-
-//let child = studentList.childNodes;
-//console.log (child);
-//console.log(studentList);
-//console.log(studentItem);
-// console.log(studentDetails);
-// console.log(studentJoined);
-
-
 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 students, the last page will only display four
@@ -34,18 +26,28 @@ let studentJoined= document.querySelectorAll('.joined-details');
 // var result = hideItems(44);
 // //console.log(result);
 //test
-const showPage = (list, firstItem, lastItem) => {
-  for (let i = 0; i <= list.length; i++) {
-    if (list[i] >= firstItem  && list[i] <= lastItem) {// if the index of a list item is >= the index of the first item that should be shown on the page, && the list item index is <= the index of the last item that should be shown on the page,
-      return list;
+const showPage = (list, firstItem,lastItem) => {
+  let child = list.children;
+  console.log(child);
+ for (let i = 0; i <= child.length; i++) {
+    if (i >= firstItem  && i <= lastItem) {
+      child[i].hidden = false;
     } else {
-
+      child[i].hidden = true;
     }
   }
 }
-
+//showPage(parent,0,2);
 // Create and append the pagination links - Creating a function that can do this is a good approach
 
+const appendPageLinks = () => {
+  showPage (parent,0,9)
+  let itemsperPage = 10;
+if () {
+  let pages = parent.length / itemsperPage;
+}
+
+}
 
 
 
